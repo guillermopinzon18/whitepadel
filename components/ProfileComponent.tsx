83addb7be6c2b91e5a275a81b8d68c0d7770641a
Guillermo Pinzon <guillermopinzon1818@gmail.com>
2025-02-25 18:58:26 -0400

    ahora si

--- components/ProfileComponent.tsx
+++ components/ProfileComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { useAuth } from "@/app/AuthContext";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
